0a4220aa482297af0023cb6b29dbb051cb21089b
kmaitski <kevin.maitski@gmail.com>
2018-05-22 17:35:51 -0700

    Add necessary code to recieve data

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/../client/public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/time', (req, res) => {
